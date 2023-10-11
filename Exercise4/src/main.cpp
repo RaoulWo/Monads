361c4eb521e83ccfcbac255d4db01992cb706c70
Raoul Wograndl <if21b142@technikum-wien.at>
2023-10-11 09:51:05 +0200

    Implement exercise 5

--- Exercise4/src/main.cpp
+++ Exercise4/src/main.cpp
@@ -1,6 +1,46 @@
+#include <functional>
 #include <iostream>
+#include <optional>
+
+template<typename TValue>
+class Maybe
+{
+    typedef std::function<std::optional<TValue>(const TValue, const TValue)> TOperation;
+public:
+    Maybe(TValue value) : value_(value) {}
+
+    std::optional<TValue> bind(const TOperation op, const TValue other)
+    {
+        if (value_ == std::nullopt || other == std::nullopt) return std::nullopt;
+
+        return op(value_.value(), other.value());
+    }
+
+private:
+    const std::optional<TValue> value_;
+};
+
+auto divide = [](int left, int right) -> std::optional<int> 
+{
+    if (right == 0) return std::nullopt;
+
+    return left / right;
+};
+
+auto print_result = [](const std::optional<int>& num) -> void
+{
+    if (num.has_value())
+    {
+        std::cout << num.value() << std::endl;
+    }
+    else 
+    {
+        std::cout << "Division by 0 not allowed" << std::endl;
+    }
+};
 
 int main()
 {
-
+    print_result(divide(10, 2));
+    print_result(divide(10, 0));
 }
--- Exercise5/src/main.cpp
+++ Exercise5/src/main.cpp
@@ -1,6 +1,64 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <optional>
+#include <string>
+
+template <typename T>
+struct Maybe
+{
+    std::optional<T> value;
+};
+
+auto read_file = [](const std::string& path) -> Maybe<std::string>
+{
+    std::ifstream stream(path);
+
+    if (!stream.is_open()) return Maybe<std::string>{std::nullopt};
+
+    std::string file;
+
+    std::string line;
+    while (std::getline(stream, line))
+    {
+        file.append(line);
+    }
+
+    return Maybe<std::string>{file};
+};
+
+auto is_ascii_char = [](const char c) -> bool
+    {
+        return !(static_cast<unsigned char>(c) > 127);
+    };
+
+auto is_ascii_string = [](const std::string& s) -> bool
+    {
+        return std::all_of(s.begin(), s.end(), is_ascii_char);
+    };
+
+auto process_file_content = [](const std::string& path) -> Maybe<std::string>
+{
+    auto file = read_file(path);
+
+    if (!file.value.has_value()) return Maybe<std::string>{std::nullopt};
+    if (!is_ascii_string(file.value.value())) return Maybe<std::string>{std::nullopt};
+
+    return Maybe<std::string>{file.value.value()};
+};
+
+
 
 int main()
 {
+    auto content = process_file_content("data/file2.txt");
 
+    if (content.value.has_value())
+    {
+        std::cout << content.value.value() << std::endl;
+    }
+    else 
+    {
+        std::cout << "Non ASCII format!" << std::endl;
+    }
 }
